60644d12f816503eef290b783862cc8b0859d782
Phil Crump <phil@philcrump.co.uk>
2020-05-30 23:00:20 +0100

    mouse.c: Use /dev/input/mice for scrollwheel, removes model-specific path.

--- mouse.c
+++ mouse.c
@@ -3,15 +3,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <linux/input.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include "timing.h"
 #include "graphics.h"
 
 /* Hardcoded, but is only one in 'by-id' directory so could use first entry in this dir? */
-char *mouse_event_path = "/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse";
+char *mouse_event_path = "/dev/input/mice";
+
+static const uint8_t mouse_intellimouse_command[] = { 0xf3, 200, 0xf3, 100, 0xf3, 80 };
+static const int32_t mouse_intellimouse_command_length = 6;
 
 /* Hz per click */
 #define SCROLL_SPEED_HIGH   5000
@@ -34,13 +39,30 @@ void *mouse_thread(void *arg)
     bool *exit_requested = (bool *)arg;
 
     int fd;
-    struct input_event event_data;
+    uint8_t mouse_buffer[4];
+    ssize_t mouse_buffer_length;
 
-    if((fd = open(mouse_event_path, O_RDONLY)) == -1)
+    if((fd = open(mouse_event_path, O_RDWR | O_NOCTTY)) == -1)
     {
         printf("Mouse: Error opening event device\n");
         return NULL;
     }
+
+    /* Switch mouse device to IntelliMouse protocol to get wheel events */
+    if(write(fd, mouse_intellimouse_command, mouse_intellimouse_command_length) != mouse_intellimouse_command_length)
+    {
+        printf("Mouse: Error sending IntelliMouse command\n");
+        close(fd);
+        return NULL;
+    }
+    if (read(fd, mouse_buffer, sizeof(mouse_buffer)) != 1 || mouse_buffer[0] != 0xFA)
+    {
+
+        printf("Mouse: Error receiving ACK for IntelliMouse command\n");
+        close(fd);
+        return NULL;
+    }
+
 
     int64_t distance_from_limit;
     uint64_t last_scroll_monotonic = 0;
@@ -49,116 +71,124 @@ void *mouse_thread(void *arg)
 
     while(false == *exit_requested)
     {
-        if(read(fd, &event_data, sizeof(event_data)))
+        mouse_buffer_length = read(fd, mouse_buffer, 4);
+        if(mouse_buffer_length == -1)
         {
-            if ((event_data.code == 8) && (event_data.type == 2) &&
-            ((event_data.value == 1) || (event_data.value == -1)))
+            if (errno == EINTR)
             {
-                if(event_data.value == 1)
+                continue;
+            }
+            printf("Mouse: Read Error: %s.\n", strerror(errno));
+            break;
+        }
+        else if (mouse_buffer_length != 4 || !(mouse_buffer[0] & 0x08))
+        {
+            /* Not a 4-byte IntelliMouse Report */
+            continue;
+        }
+        else if(mouse_buffer[3] == 0)
+        {
+            /* No wheel event here */
+            continue;
+        }
+
+        if(mouse_buffer[3] == 255)
+        {
+            /* Upwards */
+            distance_from_limit = (center_frequency + (span_frequency / 2)) - selected_center_frequency;
+
+            if(distance_from_limit <= 0)
+            {
+                continue;
+            }
+
+            if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_HIGH)
+            {
+                /* Fast scroll */
+                if(distance_from_limit < SCROLL_SPEED_HIGH)
                 {
-                    /* Upwards */
-                    distance_from_limit = (center_frequency + (span_frequency / 2)) - selected_center_frequency;
-
-                    if(distance_from_limit <= 0)
-                    {
-                        continue;
-                    }
-
-                    if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_HIGH)
-                    {
-                        /* Fast scroll */
-                        if(distance_from_limit < SCROLL_SPEED_HIGH)
-                        {
-                            selected_center_frequency += distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency += SCROLL_SPEED_HIGH;
-                        }
-                    }
-                    else if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_MEDIUM)
-                    {
-                        /* Medium scroll */
-                        if(distance_from_limit < SCROLL_SPEED_MEDIUM)
-                        {
-                            selected_center_frequency += distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency += SCROLL_SPEED_MEDIUM;
-                        }
-                    }
-                    else
-                    {
-                        /* Slow scroll */
-                        if(distance_from_limit < SCROLL_SPEED_SLOW)
-                        {
-                            selected_center_frequency += distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency += SCROLL_SPEED_SLOW;
-                        }
-                    }
-                    last_scroll_monotonic = monotonic_ms();
-                    graphics_frequency_newdata();
+                    selected_center_frequency += distance_from_limit;
                 }
-                else if(event_data.value == -1)
+                else
                 {
-                    /* Downwards */
-                    distance_from_limit = selected_center_frequency - (center_frequency - (span_frequency / 2));
-
-                    if(distance_from_limit <= 0)
-                    {
-                        continue;
-                    }
-
-                    if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_HIGH)
-                    {
-                        /* Fast scroll */
-                        if(distance_from_limit < SCROLL_SPEED_HIGH)
-                        {
-                            selected_center_frequency -= distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency -= SCROLL_SPEED_HIGH;
-                        }
-                    }
-                    else if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_MEDIUM)
-                    {
-                        /* Medium scroll */
-                        if(distance_from_limit < SCROLL_SPEED_MEDIUM)
-                        {
-                            selected_center_frequency -= distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency -= SCROLL_SPEED_MEDIUM;
-                        }
-                    }
-                    else
-                    {
-                        /* Slow scroll */
-                        if(distance_from_limit < SCROLL_SPEED_SLOW)
-                        {
-                            selected_center_frequency -= distance_from_limit;
-                        }
-                        else
-                        {
-                            selected_center_frequency -= SCROLL_SPEED_SLOW;
-                        }
-                    }
-                    last_scroll_monotonic = monotonic_ms();
-                    graphics_frequency_newdata();
+                    selected_center_frequency += SCROLL_SPEED_HIGH;
                 }
             }
-#if 1
-            printf("time %ld.%06ld: Value=%d\n",
-                event_data.time.tv_sec, event_data.time.tv_usec,
-                event_data.value
-            );
-#endif
+            else if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_MEDIUM)
+            {
+                /* Medium scroll */
+                if(distance_from_limit < SCROLL_SPEED_MEDIUM)
+                {
+                    selected_center_frequency += distance_from_limit;
+                }
+                else
+                {
+                    selected_center_frequency += SCROLL_SPEED_MEDIUM;
+                }
+            }
+            else
+            {
+                /* Slow scroll */
+                if(distance_from_limit < SCROLL_SPEED_SLOW)
+                {
+                    selected_center_frequency += distance_from_limit;
+                }
+                else
+                {
+                    selected_center_frequency += SCROLL_SPEED_SLOW;
+                }
+            }
+            last_scroll_monotonic = monotonic_ms();
+            graphics_frequency_newdata();
+        }
+        else if(mouse_buffer[3] == 1)
+        {
+            /* Downwards */
+            distance_from_limit = selected_center_frequency - (center_frequency - (span_frequency / 2));
+
+            if(distance_from_limit <= 0)
+            {
+                continue;
+            }
+
+            if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_HIGH)
+            {
+                /* Fast scroll */
+                if(distance_from_limit < SCROLL_SPEED_HIGH)
+                {
+                    selected_center_frequency -= distance_from_limit;
+                }
+                else
+                {
+                    selected_center_frequency -= SCROLL_SPEED_HIGH;
+                }
+            }
+            else if(monotonic_ms() - last_scroll_monotonic < SCROLL_TIME_MEDIUM)
+            {
+                /* Medium scroll */
+                if(distance_from_limit < SCROLL_SPEED_MEDIUM)
+                {
+                    selected_center_frequency -= distance_from_limit;
+                }
+                else
+                {
+                    selected_center_frequency -= SCROLL_SPEED_MEDIUM;
+                }
+            }
+            else
+            {
+                /* Slow scroll */
+                if(distance_from_limit < SCROLL_SPEED_SLOW)
+                {
+                    selected_center_frequency -= distance_from_limit;
+                }
+                else
+                {
+                    selected_center_frequency -= SCROLL_SPEED_SLOW;
+                }
+            }
+            last_scroll_monotonic = monotonic_ms();
+            graphics_frequency_newdata();
         }
     }
 
